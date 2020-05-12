717bde2c5b4f12e45f6589108a8b2d6081662b49
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-05-12 19:53:24 +0900

    Update index.js

--- asset/js/index.js
+++ asset/js/index.js
@@ -13,8 +13,6 @@ function handleKeyEvent(e) {
     const tab = document.querySelector(`.js-key[data-key="${e.keyCode}"]`);
     const shift_L = document.querySelector(`.${SHIFT_L}[data-key="${e.keyCode}"]`);;
     const shift_R = document.querySelector(`.${SHIFT_R}[data-key="${e.keyCode}"]`);;
-
-
     if(e.keyCode === 16 && e.location === 1){
         box = shift_L.parentNode;
     }else if(e.keyCode === 16 && e.location === 2){
@@ -31,7 +29,19 @@ function handleKeyEvent(e) {
 function init(){
     window.addEventListener("keydown", handleKeyEvent); 
     window.addEventListener("keyup", function(e){
-        box.classList.remove(TAB);
+        const tab = document.querySelector(`.js-key[data-key="${e.keyCode}"]`);
+        const shift_L = document.querySelector(`.${SHIFT_L}[data-key="${e.keyCode}"]`);;
+        const shift_R = document.querySelector(`.${SHIFT_R}[data-key="${e.keyCode}"]`);;
+        let keyBox;
+        if(e.keyCode === 16 && e.location === 1){
+            keyBox = shift_L.parentNode;
+        }else if(e.keyCode === 16 && e.location === 2){
+            keyBox = shift_R.parentNode;
+        }else {
+            keyBox = tab.parentNode;
+        }
+        console.log(keyBox);
+        keyBox.classList.remove(TAB);
     });
 }
 
