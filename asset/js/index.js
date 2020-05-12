db434d8d7d2f839eae914448412b24d683598fef
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-05-12 19:45:37 +0900

    Update index.js

--- asset/js/index.js
+++ asset/js/index.js
@@ -2,18 +2,19 @@ const TAB = "js-tab";
 const SHIFT_L = "js-shift-l";
 const SHIFT_R = "js-shift-r";
 
-function handleTab(element) {
-    element.addEventListener("transitionend", function(e){
-        element.classList.remove(TAB);
-    });
-}
+// function handleTab(element) {
+//     element.addEventListener("keyup", function(e){
+//         element.classList.remove(TAB);
+//     });
+// }
+let box;
 
 function handleKeyEvent(e) {
     const tab = document.querySelector(`.js-key[data-key="${e.keyCode}"]`);
     const shift_L = document.querySelector(`.${SHIFT_L}[data-key="${e.keyCode}"]`);;
     const shift_R = document.querySelector(`.${SHIFT_R}[data-key="${e.keyCode}"]`);;
 
-    let box;
+
     if(e.keyCode === 16 && e.location === 1){
         box = shift_L.parentNode;
     }else if(e.keyCode === 16 && e.location === 2){
@@ -21,13 +22,17 @@ function handleKeyEvent(e) {
     }else {
         box = tab.parentNode;
     }
+
     e.preventDefault();
     box.classList.add(TAB);
-    handleTab(box);
+    // handleTab(box);
 }
 
 function init(){
     window.addEventListener("keydown", handleKeyEvent); 
+    window.addEventListener("keyup", function(e){
+        box.classList.remove(TAB);
+    });
 }
 
 init();
